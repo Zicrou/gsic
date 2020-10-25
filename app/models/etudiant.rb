4fc16ca3c116754253dcf8c6e0393d2d2eb97d64
Zicrou <aazseck@gmail.com>
2020-10-25 10:08:16 +0000

    Refactoring

--- app/models/etudiant.rb
+++ app/models/etudiant.rb
@@ -7,7 +7,7 @@ class Etudiant < ApplicationRecord
     belongs_to :province, optional: true
     belongs_to :langueformation
     belongs_to :user
-    has_one :zone
+    belongs_to :zone, optional: true
 
     validates_presence_of :nom, :prenom, :date_nasissance, :genre_id, :telephone, :passport, :date_passport_expiration, :carteconsulaire_id, :adresse, :boursier_id, :faireanneelangue_id, :annee_debut_filiere, :province_uinversite_filiere, :universite_filiere, :filiere, :langueformation_id, :duree_formation, :annee_de_graduation, :niveau_formation, :passport_image, :photo, :etreenchine, :email, :user_id
 
@@ -17,5 +17,9 @@ class Etudiant < ApplicationRecord
 
     def self.etudiant_zone(province)
         Etudiant.where(name)
+    end
+
+    def self.by_zone()
+        Etudiant.where()
     end
 end
