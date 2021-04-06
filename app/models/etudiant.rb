d66c785520fed624bbfb87b05e11d59dcd893e19
Zicrou <aazseck@gmail.com>
2021-04-06 02:01:06 +0000

    Added Active Storage For Image Managing

--- app/models/etudiant.rb
+++ app/models/etudiant.rb
@@ -11,6 +11,8 @@ class Etudiant < ApplicationRecord
 
     validates_presence_of :nom, :prenom, :date_nasissance, :genre_id, :telephone, :passport, :date_passport_expiration, :carteconsulaire_id, :adresse, :boursier_id, :faireanneelangue_id, :annee_debut_filiere, :universite_filiere, :filiere, :langueformation_id, :duree_formation, :annee_de_graduation, :niveau_formation, :passport_image, :photo, :etreenchine, :email, :user_id # :province_uinversite_filiere,
 
+    has_one_attached :passport_image
+    has_one_attached :photo
     #mount_uploader :passport_image, EtudiantUploader
     #mount_uploader :photo, EtudiantUploader
 
